75b00439a4c099e9889a75cdd5349e25e71640db
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-01-08 11:12:01 +0530

    reactday3

---
+++ day3/react-app/src/App.jsx
@@ -0,0 +1,46 @@
+
+
+const App = () => {// functional component  // component name start with capital letter
+
+
+  return (
+    <div>
+    <div>
+      <h1>hello world</h1>
+      <p>batman</p>
+      
+    </div>
+    <div>
+      <h1>hello world</h1>
+      <p>superman</p>
+    </div>
+    </div>
+  )
+}
+
+export default App
+rafc
+
+// rafce   export default function create 
+//rafc export function create
+
+
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <div>App</div>
+//   )
+// }
+
+// export default App
+
+// import React from 'react'
+
+// export const App = () => {
+//   return (
+//     <div>App</div>
+//   )
+// }
+
+
