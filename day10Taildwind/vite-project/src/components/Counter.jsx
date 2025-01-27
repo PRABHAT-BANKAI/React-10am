1bb4aa3ff6401b9ad03f43851c7b31cf33d88409
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-01-27 12:50:01 +0530

    fix

--- day10Taildwind/vite-project/src/components/Counter.jsx
+++ day10Taildwind/vite-project/src/components/Counter.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 
 const Counter = () => {
-  const [count, setCount] = useState();
+  const [count, setCount] = useState(
+    JSON.parse(localStorage.getItem("counter")) || 0
+  );
 
   useEffect(() => {
-    localStorage.setItem("counter", count);
+    localStorage.setItem("counter", JSON.stringify(count));
   });
 
   return (
