1655a3468e0a9285c4792769ff95145ebc57d575
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-02-13 10:59:35 +0530

    crud

---
+++ day16todo/tod/src/App.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { AddTodo, updateTodo } from "./redux/features/todolistSlice";
+
+function App() {
+  const [inputText, setText] = useState("");
+  const [boolean, setBoolean] = useState(true);
+  const [todoIndex, setTodoIndex] = useState(null);
+  const dispatch = useDispatch();
+  const todoData = useSelector((state) => state.todolist.todos);
+
+  function handleEdit(index) {
+    setText(todoData[index]);
+    setTodoIndex(index);
+    setBoolean(false);
+  }
+  function handleUpdate() {
+    dispatch(updateTodo({ id: todoIndex, updateText: inputText }));
+    setBoolean(true);
+    setText("")
+  }
+
+  function handleAdd() {
+    dispatch(AddTodo(inputText));
+    setText("");
+  }
+  return (
+    <>
+      <h1>Todolist</h1>
+
+      <input
+        type="text"
+        value={inputText}
+        onChange={(event) => {
+          setText(event.target.value);
+        }}
+      />
+      {boolean ? (
+        <button onClick={handleAdd}>Add</button>
+      ) : (
+        <button onClick={handleUpdate}>Update</button>
+      )}
+
+      <div>
+        {todoData &&
+          todoData.map((todo, index) => {
+            return (
+              <div key={index}>
+                <p>{todo}</p>
+                <button
+                  onClick={() => {
+                    handleEdit(index);
+                  }}
+                >
+                  edit
+                </button>
+                <button>delete</button>
+              </div>
+            );
+          })}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ day16todo/tod/src/redux/features/todolistSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const todolistSlice = createSlice({
+  name: "todolist",
+  initialState: {
+    todos: JSON.parse(localStorage.getItem("todolistData")) ||[],
+  },
+  reducers: {
+    AddTodo: (state, action) => {
+      state.todos = [...state.todos, action.payload];
+      localStorage.setItem("todolistData", JSON.stringify(state.todos));
+    },
+    updateTodo: (state, action) => {
+      console.log(action.payload.updateText)
+      console.log(action.payload.id)
+      state.todos = state.todos.map((todo) =>{
+        console.log(action.payload)
+      return  todo.id === action.payload.id ? action.payload.updateText : todo
+      }
+       
+      );
+      localStorage.setItem("todolistData", JSON.stringify(state.todos));
+    },
+    deleteTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { AddTodo, deleteTodo, updateTodo } = todolistSlice.actions;
+
+export default todolistSlice.reducer;
