7f12253a5f077c18be85921ade3190e8d2701cd0
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-03-05 10:47:35 +0530

    todolist

--- day23firbaseCrud/todo/src/App.jsx
+++ day23firbaseCrud/todo/src/App.jsx
@@ -4,6 +4,7 @@ import {
   deleteDoc,
   doc,
   getDocs,
+  updateDoc,
 } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db } from "./config/firbase";
@@ -32,10 +33,34 @@ const App = () => {
       }))
     );
   }
+  async function handleEdit(todo) {
+    // const updateValue =  doc(database, todo.id,);
+    setInputText(todo.todo);
+    setBoolean(true);
+    setEditId(todo.id);
+  }
+  async function handleUpdate() {
+    if (!inputText.trim()) {
+      alert("Please enter your first field ");
+      return;
+    }
+    const updateValue = doc(database, editId);
+    await updateDoc(updateValue, { todo: inputText });
+    alert("your todo updated successfully");
+    setInputText("");
+    setBoolean(false);
+    const getTodos = await getDocs(database);
+    setTodolist(
+      getTodos.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+    );
+  }
 
   async function hanldeDelete(id) {
     console.log(id);
-    const DeleteValue = doc(database,  id);
+    const DeleteValue = doc(database, id);
     await deleteDoc(DeleteValue);
 
     setTodolist((prev) => prev.filter((doc) => doc.id != id));
@@ -64,7 +89,7 @@ const App = () => {
         onChange={(e) => setInputText(e.target.value)}
       />
       {boolean ? (
-        <button>Update</button>
+        <button onClick={handleUpdate}>Update</button>
       ) : (
         <button onClick={handleAdd}>Add</button>
       )}
@@ -74,7 +99,13 @@ const App = () => {
           todolist.map((item) => (
             <div key={item.id}>
               <p>{item.todo}</p>
-              <button>Edit</button>
+              <button
+                onClick={() => {
+                  handleEdit(item);
+                }}
+              >
+                Edit
+              </button>
               <button
                 onClick={() => {
                   hanldeDelete(item.id);
